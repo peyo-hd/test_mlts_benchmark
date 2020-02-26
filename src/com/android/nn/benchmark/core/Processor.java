6387cda582c31d8cbc98a7f2eef64c10a90d9eb4
Stefano Galarraga <galarragas@google.com>
2020-02-26 14:35:51 +0000

    Refactor code to prepare for parallel tests
    Removing some of the old-level Java syncrhonisation to use higher
    level java.concurrency constructs.
    Removing some unused code.
    
    Test: build_and_run_benchmark.sh scoring
    
    Change-Id: I010b6cec8a43ce8192b30cd71cd5ae6fad227e5c

--- src/com/android/nn/benchmark/core/Processor.java
+++ src/com/android/nn/benchmark/core/Processor.java
@@ -16,6 +16,8 @@
 
 package com.android.nn.benchmark.core;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
 import android.content.Context;
 import android.os.Trace;
 import android.util.Log;
@@ -23,21 +25,26 @@ import android.util.Pair;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /** Processor is a helper thread for running the work without blocking the UI thread. */
-public class Processor extends Thread {
+public class Processor implements Runnable {
 
     public interface Callback {
-        public void onBenchmarkFinish(boolean ok);
+        void onBenchmarkFinish(boolean ok);
 
-        public void onStatusUpdate(int testNumber, int numTests, String modelName);
+        void onStatusUpdate(int testNumber, int numTests, String modelName);
     }
 
     protected static final String TAG = "NN_BENCHMARK";
     private Context mContext;
 
-    private float mLastResult;
-    private boolean mRun = true;
+    private final AtomicBoolean mRun = new AtomicBoolean(true);
+
+    volatile boolean mHasBeenStarted = false;
+    // You cannot restart a thread, so the completion flag is final
+    private final CountDownLatch mCompleted = new CountDownLatch(1);
     private boolean mDoingBenchmark;
     private NNTestBase mTest;
     private int mTestList[];
@@ -78,18 +85,24 @@ public class Processor extends Thread {
     // Method to retrieve benchmark results for instrumentation tests.
     public BenchmarkResult getInstrumentationResult(
             TestModels.TestModelEntry t, float warmupTimeSeconds, float runTimeSeconds)
-            throws IOException {
+            throws IOException, BenchmarkException {
         mTest = changeTest(mTest, t);
-        return getBenchmark(warmupTimeSeconds, runTimeSeconds);
+        BenchmarkResult result = getBenchmark(warmupTimeSeconds, runTimeSeconds);
+        mTest.destroy();
+        mTest = null;
+        return result;
     }
 
-    private NNTestBase changeTest(NNTestBase oldTestBase, TestModels.TestModelEntry t) {
+    private NNTestBase changeTest(NNTestBase oldTestBase, TestModels.TestModelEntry t)
+            throws BenchmarkException {
         if (oldTestBase != null) {
             // Make sure we don't leak memory.
             oldTestBase.destroy();
         }
         NNTestBase tb = t.createNNTestBase(mUseNNApi, false /* enableIntermediateTensorsDump */);
-        tb.setupModel(mContext);
+        if (!tb.setupModel(mContext)) {
+            throw new BenchmarkException("Cannot initialise model");
+        }
         return tb;
     }
 
@@ -133,13 +146,9 @@ public class Processor extends Thread {
             mTest.checkSdkVersion();
         } catch (UnsupportedSdkException e) {
             BenchmarkResult r = new BenchmarkResult(e.getMessage());
-            Log.v(TAG, "Test: " + r.toString());
+            Log.v(TAG, "Unsupported SDK for test: " + r.toString());
             return r;
         }
-
-        mDoingBenchmark = true;
-
-        long result = 0;
 
         // We run a short bit of work before starting the actual test
         // this is to let any power management do its job and respond.
@@ -163,79 +172,114 @@ public class Processor extends Thread {
             Trace.endSection();
         }
 
-        Log.v(TAG, "Test: " + r.toString());
+        Log.v(TAG, "Completed benchmark loop");
 
-        mDoingBenchmark = false;
         return r;
     }
 
     @Override
     public void run() {
-        while (mRun) {
-            // Our loop for launching tests or benchmarks
-            synchronized (this) {
-                // We may have been asked to exit while waiting
-                if (!mRun) return;
-            }
-
-            try {
-                // Loop over the tests we want to benchmark
-                for (int ct = 0; (ct < mTestList.length) && mRun; ct++) {
-
-                    // For reproducibility we wait a short time for any sporadic work
-                    // created by the user touching the screen to launch the test to pass.
-                    // Also allows for things to settle after the test changes.
-                    try {
-                        sleep(250);
-                    } catch (InterruptedException e) {
-                    }
-
-                    TestModels.TestModelEntry testModel =
-                            TestModels.modelsList().get(mTestList[ct]);
-                    int testNumber = ct + 1;
-                    mCallback.onStatusUpdate(testNumber, mTestList.length, testModel.toString());
-
-                    // Select the next test
-                    mTest = changeTest(mTest, testModel);
-
-                    // If the user selected the "long pause" option, wait
-                    if (mTogglePause) {
-                        for (int i = 0; (i < 100) && mRun; i++) {
-                            try {
-                                sleep(100);
-                            } catch (InterruptedException e) {
-                            }
-                        }
-                    }
-
-                    // Run the test
-                    float warmupTime = 0.3f;
-                    float runTime = 1.f;
-                    if (mToggleLong) {
-                        warmupTime = 2.f;
-                        runTime = 10.f;
-                    }
-                    mTestResults[ct] = getBenchmark(warmupTime, runTime);
+        mHasBeenStarted = true;
+        Log.d(TAG, "Processor starting");
+        try {
+            while (mRun.get()) {
+                try {
+                    benchmarkAllModels();
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException during benchmark run", e);
+                    break;
+                } catch (Throwable e) {
+                    Log.e(TAG, "Error during execution", e);
+                    throw e;
                 }
-                mCallback.onBenchmarkFinish(mRun);
-            } catch (IOException e) {
-                Log.e(TAG, "Exception during benchmark run", e);
+
+                mCallback.onBenchmarkFinish(mRun.get());
+            }
+        } finally {
+            mCompleted.countDown();
+        }
+    }
+
+    private void benchmarkAllModels() throws IOException {
+        Log.i(TAG, String.format("Iterating through %d models", mTestList.length));
+        // Loop over the tests we want to benchmark
+        for (int ct = 0; ct < mTestList.length; ct++) {
+            if (!mRun.get()) {
+                Log.v(TAG, String.format("Asked to stop execution at model #%d", ct));
                 break;
             }
+            // For reproducibility we wait a short time for any sporadic work
+            // created by the user touching the screen to launch the test to pass.
+            // Also allows for things to settle after the test changes.
+            try {
+                Thread.sleep(250);
+            } catch (InterruptedException ignored) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+
+            TestModels.TestModelEntry testModel =
+                    TestModels.modelsList().get(mTestList[ct]);
+
+            Log.i(TAG, String.format("%d/%d: '%s'", ct, mTestList.length,
+                    testModel.mTestName));
+            int testNumber = ct + 1;
+            mCallback.onStatusUpdate(testNumber, mTestList.length,
+                    testModel.toString());
+
+            // Select the next test
+            try {
+                mTest = changeTest(mTest, testModel);
+            } catch (BenchmarkException e) {
+                Log.w(TAG, String.format("Cannot initialise test %d: '%s', skipping", ct,
+                        testModel.mTestName), e);
+            }
+
+            // If the user selected the "long pause" option, wait
+            if (mTogglePause) {
+                for (int i = 0; (i < 100) && mRun.get(); i++) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ignored) {
+                        Thread.currentThread().interrupt();
+                        break;
+                    }
+                }
+            }
+
+            // Run the test
+            float warmupTime = 0.3f;
+            float runTime = 1.f;
+            if (mToggleLong) {
+                warmupTime = 2.f;
+                runTime = 10.f;
+            }
+            Log.i(TAG, "Running test for model " + testModel.mModelName + " file "
+                    + testModel.mModelFile);
+            mTestResults[ct] = getBenchmark(warmupTime, runTime);
         }
     }
 
     public void exit() {
-        mRun = false;
+        exitWithTimeout(-1l);
+    }
 
-        synchronized (this) {
-            notifyAll();
-        }
-        // exit() is called on same thread when run via dogfood BenchmarkJobService
-        if (this != Thread.currentThread()) {
+    public void exitWithTimeout(long timeoutMs) {
+        mRun.set(false);
+
+        if (mHasBeenStarted) {
             try {
-                this.join();
+                if (timeoutMs > 0) {
+                    boolean hasCompleted = mCompleted.await(timeoutMs, MILLISECONDS);
+                    if (!hasCompleted) {
+                        Log.w(TAG, "Exiting before execution actually completed");
+                    }
+                } else {
+                    mCompleted.await();
+                }
             } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                Log.w(TAG, "Interrupted while waiting for Processor to complete", e);
             }
         }
 
